1d0be40929298d2af6760540ee61064e77657a03
Scott Aubrey <scott@aubrey.org.uk>
2023-10-23 10:31:21 +0100

    make the test work

--- integration-tests/api.spec.ts
+++ integration-tests/api.spec.ts
@@ -1,22 +1,26 @@
 import request from 'supertest';
+import { readFileSync } from 'fs';
 import app from '../src/app';
 
 describe('api', () => {
   describe('/', () => {
     describe('version selection version', () => {
+      const xml = readFileSync('integration-tests/data/10.1101/123456/123456.xml');
       it('should select the latest version', async () => {
         await request(app)
           .post('/')
-          .expect(200)
-          .expect({ version: '1.0.3' });
+          .send(xml.toString())
+          // .expect(200)
+          .expect((response) => expect(response.body).toMatchObject({ version: '1.0.3' }));
       });
 
       it('should use the specified version', async () => {
         await request(app)
           .post('/')
           .set('Accept', 'application/vnd.elife.encoda.v1.0.1+json')
-          .expect(200)
-          .expect({ version: '1.0.1' });
+          .send(xml.toString())
+          // .expect(200)
+          .expect((response) => expect(response.body).toMatchObject({ version: '1.0.1' }));
       });
     });
   });
