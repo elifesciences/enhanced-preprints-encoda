2e472c9ab8b4e1472087bb73b3e3f527cf215a2d
Nathan Lisgo <nathan@lisgo.co.uk>
2024-02-09 10:31:45 +0000

    Return only the json for the body of the response

--- src/app.ts
+++ src/app.ts
@@ -33,24 +33,15 @@ app.post('/', async (req, res) => {
 
     const versionResponders = {
       'application/vnd.elife.encoda.v1.0.3+json': async () => {
-        res.json({
-          version: '1.0.3',
-          json: (await convert_1_0_3(xmlFile, undefined, parameters))?.replaceAll(tempOutput, replacementPath),
-        });
+        res.json(JSON.parse(((await convert_1_0_3(xmlFile, undefined, parameters)) ?? '{}').replaceAll(tempOutput, replacementPath)));
         rmdirSync(tempOutput, { recursive: true });
       },
       'application/vnd.elife.encoda.v1.0.2+json': async () => {
-        res.json({
-          version: '1.0.2',
-          json: (await convert_1_0_2(xmlFile, undefined, parameters))?.replaceAll(tempOutput, replacementPath),
-        });
+        res.json(JSON.parse(((await convert_1_0_2(xmlFile, undefined, parameters)) ?? '{}').replaceAll(tempOutput, replacementPath)));
         rmdirSync(tempOutput, { recursive: true });
       },
       'application/vnd.elife.encoda.v1.0.1+json': async () => {
-        res.json({
-          version: '1.0.1',
-          json: (await convert_1_0_1(xmlFile, undefined, parameters))?.replaceAll(tempOutput, replacementPath),
-        });
+        res.json(JSON.parse(((await convert_1_0_1(xmlFile, undefined, parameters)) ?? '{}').replaceAll(tempOutput, replacementPath)));
         rmdirSync(tempOutput, { recursive: true });
       },
       default: async () => versionResponders['application/vnd.elife.encoda.v1.0.3+json'](),
